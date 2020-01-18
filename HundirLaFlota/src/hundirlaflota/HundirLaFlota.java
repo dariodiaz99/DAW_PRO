4bc2d1338ec304d1637953fb9ba1bcc4067ddd33
Dario <Dario@DESKTOP-PF3MOFS>
2020-01-18 13:19:40 +0000

    creacion del menu

--- HundirLaFlota/src/hundirlaflota/HundirLaFlota.java
+++ HundirLaFlota/src/hundirlaflota/HundirLaFlota.java
@@ -5,6 +5,9 @@
  */
 package hundirlaflota;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author Dario
@@ -15,7 +18,45 @@ public class HundirLaFlota {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Menu del juego
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion; //Guardaremos la opcion del usuario
+ 
+        while (!salir) { //Mientras sea distinto a salir hacer lo siguiente
+            
+            System.out.println("Escribe una de las opciones a elegir");
+            
+            System.out.println("1. Insertar Barco 1 (Tamaño: 1)");
+            System.out.println("2. Insertar Barco 2 (Tamaño: 2)");
+            System.out.println("3. Salir");
+ 
+            try {
+ 
+                System.out.println("Escribe una de las opciones");
+                opcion = sn.nextInt();
+ 
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Has seleccionado el 1");
+                        break;
+                    case 2:
+                        System.out.println("Has seleccionado el 2");
+                        break;
+                    case 3:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 3");
+                }
+            } catch (InputMismatchException e) {
+                //ponemos excepcion para que no exista problemas con una 
+                //cadena y sea obligado utilizar numeros.
+                System.out.println("Debes insertar un número");
+                sn.next();
+            }
+        }
+ 
     }
-    
 }
+ 
